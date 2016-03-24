5891e400c73e5250181ebf8dd3d01451f4497edc
Katherine Huang <latias320@gmail.com>
2016-03-24 01:45:58 -0400

    dear god

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -3,9 +3,15 @@ package com.flatironschool.javacs;
 public class HelloWorld {
     public static Double getVersion() {
         // I won't work!
+        String javaver;
+        Double java_num;
+        javaver = System.getProperty("java.specification.version");
+        java_num = Double.parseDouble(javaver);
+        return java_num;
     }
 
     public static void main(String[] args) {
 	    // you can test the output of getVersion() here
+      System.out.println(getVersion());
     }
 }
